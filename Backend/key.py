17a63f424914eb9d346cee7f5ef586447f50ab80
Tutu Daranijo <tutudaranijo@gmail.com>
2024-03-24 21:30:34 -0500

    update

--- Backend/key.py
+++
@@ -1,5 +0,0 @@
-MONGOURIPA= 'Me21TnJCgk7fE224'
-MONGOURI= f"mongodb+srv://tutudaranijo:{MONGOURIPA}@mongoqa.glt6ulv.mongodb.net/?retryWrites=true&w=majority&appName=MongoQA"
-openAI='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'
-hugembed='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'
-embedding_url ="https://api-inference.huggingface.co/models/radames/all-MiniLM-L6-v2-feature-extraction"
